d028bdfb3356dbb52483284936fa9aa3012199a3
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-08-08 11:49:08 -0300

    Live coding 1 y 4

---
+++ R_INTA_LC1_2019.R
@@ -0,0 +1,72 @@
+### LIVE CODING
+
+# 1- Explicar RStudio
+
+# Tiene 4 paneles,
+# a la izq abajo la consola de R (corro código con control + Enter)
+# por ejemplo:
+
+# suma
+2+2
+
+# a la izq arriba el editor (uso de # para silenciar una linea)
+# a la derecha arriba, Environment (donde puedo ver lo que 
+# guardo cuando asignó una variable)
+# a la derecha abajo puedo ver los paquetes instalados ("Packages"),
+# la ayuda ("Help"), la pestaña para visualizar gráficos ("Plots"),
+# visualizas los archivos que hay en la carpeta en ("Files")
+
+# 2- Abrir un proyecto
+
+# Ventajas: 1 - Pestaña "files" me permite ver los archivos de la 
+# carpeta desde RStudio
+# 2 - Todo se guarda en la misma carpeta
+# 3 - Uso ubicaciones relativas de los archivos. Me permite poder correr
+# el código cuando llevo la carpeta de un lugar a otro
+# 4 - Parte derecha superior de RStudio tiene un menu desplegable que me 
+# permite cambiar de proyecto o cerrarlo 
+
+
+# 3- Abro un script y lo guardo
+# FILE > NEW FILE > R SCRIPT
+# Muestro que el codigo se puede guardar y que se ve en la pestaña "File"
+# Asigno una variable
+
+suma <- 2+2
+
+# 4- Instalar un paquete y cargar la library
+
+install.packages("dplyr")
+library(dplyr)
+
+
+# 5- importar archivos de Excel con RStudio ("Import Dataset")
+# 6- importar archivos de Excel con R (como código). Ver panel
+# code preview de la pestaña que aparece cuando quiero importar desde
+# RStudio
+
+# ventaja de escribir el código:queda registrada la ubicacion del archivo
+# puedo subir muchos archivos
+
+# 7- Generar una dataframe en R con 
+data.frame()
+
+# Uso de View(). Guardarlo en el Environment
+# cuidado al elegir el nombre
+# 8- Uso de la ayuda. Introduccion al uso de vectores
+
+?c()
+
+#En la ayuda de c() hay vectores de ejemplo que corren bien
+c(1,7:9)
+c(1:5, 10.5, "next")
+
+# 9 - Los guardo en el environment y obligo que lea al ultimo
+# como numero
+
+vector <- c(1:5, 10.5, "next")
+as.numeric(vector)
+
+# que ocurre? Esto podria pasarme cuando leo un dataset
+# es importante porque puede afectar lo que pasa cuando quiero
+# ejemplo usando base de datos "muestreo dic 2012" 
---
+++ R_INTA_LC4_2019.R
@@ -0,0 +1,43 @@
+
+# web apps, graficos interactivos, mapas en pocas lineas
+
+library(ggplot2)
+library(dplyr)
+
+# Elimino los datos hago esto para no usar todos los datos 
+# y que no tarde tanto en cargar es solo un ejemplo
+
+diam_1500 <- diamonds %>%
+  filter(cut == "Ideal" & price > 15000)
+
+# Realizo un gráfico con ggplot2 
+
+ggplot(diam_1500)+
+  geom_point(aes(x = carat, y = price, color = clarity))
+
+#### PLOTLY: Graficos interactivos ####
+# uso la funcion ggplotly() en el gráfico anterior
+
+library(plotly)
+
+ggplotly(ggplot(diam_1500)+
+         geom_point(aes(x = carat, y = price, color = clarity)))
+
+#### MAPA con ggplot2 ####
+
+# bajo datos
+mapa <- ggplot2::map_data("world")
+
+# grafico
+ggplot(data = mapa)+
+  geom_polygon(aes(x = long, y = lat, group = group))+ # capa de mapa
+  geom_point(aes(x = -65, y = -33), color = "red", size = 10) # punto
+
+#### SHINY: Web apps con R ####
+
+# NEW FILE > SHINY WEB APP... > 
+
+# Tocar "Run App" en el editor para correr el ejemplo predeterminado
+
+
+
